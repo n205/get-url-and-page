5ab07685b233be64d7b4c071ddf81186bf85bae5
n205 <54013649+n205@users.noreply.github.com>
2025-06-21 09:38:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def main():
     print('📥 リクエストを受け取りました')
-    return 'Cloud Run Function executed.', 2000
+    return 'Cloud Run Function executed.', 200
 
 if __name__ == '__main__':
     print('🚀 アプリを起動します')
