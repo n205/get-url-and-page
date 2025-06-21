86a80fb90fec59761d9a16b0bd23ab61c3ad79e5
n205 <54013649+n205@users.noreply.github.com>
2025-06-21 09:51:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from flask import Flask
+import logging
+
+# Cloud Logging に出力するよう設定
+logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-    print('📥 リクエストを受け取りました')
+    logging.info('📥 リクエストを受け取りました')
     return 'Cloud Run Function executed.', 200
 
 if __name__ == '__main__':
-    print('🚀 アプリを起動します')
+    logging.info('🚀 アプリを起動します')
     app.run(host='0.0.0.0', port=8080)
